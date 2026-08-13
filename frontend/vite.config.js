b85bf8c82890b4aa874d8ed4316d3c3bdd7b92d3
Hemavathi <aswinidevika84@gmail.com>
2026-08-14 00:41:38 +0530

    Fix Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['smart-campus-project-web.onrender.com'],
+  server: {
+    port: 5173,
   },
-})
+  preview: {
+    allowedHosts: ["smart-campus-project-web.onrender.com"],
+  },
+});
